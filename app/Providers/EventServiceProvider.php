df3a81c5319860205e7d9e223e5752b84ffede02
stefan <stefan.filip.005@gmail.com>
2023-03-17 02:12:55 +0100

    handle redirect correctly in Event:listen

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -31,13 +31,15 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
         Event::listen(\Slides\Saml2\Events\SignedIn::class, function (\Slides\Saml2\Events\SignedIn $event) {
-            //$messageId = $event->auth->getLastMessageId();
+            $messageId = $event->auth->getLastMessageId();
             $samlUser = $event->auth->getSaml2User();
             $attributes = $samlUser->getAttributes();
             if($employee = Employee::where('remoteId',$attributes['mnr'][0])->first()){
-                return redirect('https://intern.rkhl.at?token='.$employee->createToken("API TOKEN")->plainTextToken);
+                abort(redirect('https://intern.rkhl.at?token='.$employee->createToken("API TOKEN")->plainTextToken));
+				exit;
             }
-            exit;
+            abort(redirect('https://intern.rkhl.at'));
+			exit;
         });
     }
 }
